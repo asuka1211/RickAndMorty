02d6eb14c71b22d78b6762d24c6a9535978a77bd
asuka1211 <maks1211@bk.ru>
2021-08-29 17:10:28 +0300

    add: change package name + update dependency

--- app/src/main/java/com/serma/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/serma/rickandmorty/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.rickandmorty
+package com.serma.rickandmorty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/test/java/com/serma/rickandmorty/ExampleUnitTest.kt
+++ app/src/test/java/com/serma/rickandmorty/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.rickandmorty
+package com.serma.rickandmorty
 
 import org.junit.Test
 
